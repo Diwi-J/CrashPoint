95cfc6b21b92553e8161e6189cd5999fa69b3e6c
Diwi-J <2589474@students.wits.ac.za>
2025-05-16 11:43:52 +0200

    Committing Player Controller
    Created the player controller.
    
    *Player will walk with "WASD"
    *Player will Crouch with "LeftControl" and Run with "LeftShfit"
    *Player will face the mouse direction at all times
    *Player animates while moving.

--- Assets/Scripts/Player/PlayerControler.cs
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -1,46 +1,112 @@
+using System.Collections;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Player Movement")]
-    [SerializeField] float walkSpeed = 40f;
-    [SerializeField] float runSpeed = 60f;
-    [SerializeField] float crouchSpeed = 20f;
+    [Header("Movement Setting")]
+    [SerializeField] float WalkSpeed = 3f;
+    [SerializeField] float RunSpeed = 5f;
+    [SerializeField] float CrouchSpeed = 1.5f;
 
-    float currentSpeed;
+    float MoveSpeed;
+
+    public enum MoveState
+    {
+        Walk,
+        Run,
+        Crouch
+    }
+    MoveState CurrentsMoveState = MoveState.Walk;
+
     Rigidbody2D rb;
     Vector2 movement;
 
-    void Start()
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    [Space]
+    [Header("Sprite Settings")]
+    [SerializeField] Sprite IdleSprite;
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        // Get movement input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        // Handle speed changes
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            currentSpeed = runSpeed;
-        }
-        else if (Input.GetKey(KeyCode.LeftControl))
-        {
-            currentSpeed = crouchSpeed;
-        }
-        else
-        {
-            currentSpeed = walkSpeed;
-        }
+        MoveInput();
+        RotationInput();
+        Animate();
     }
 
     void FixedUpdate()
     {
-        // Apply movement
-        rb.MovePosition(rb.position + movement * currentSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * MoveSpeed * Time.deltaTime);
+    }
+
+    void MoveInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(movement.x, movement.y).normalized;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            CurrentsMoveState = MoveState.Run;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            CurrentsMoveState = MoveState.Crouch;
+        }
+        else
+        {
+            CurrentsMoveState = MoveState.Walk;
+        }
+
+        switch (CurrentsMoveState)
+        {
+            case MoveState.Run:
+                MoveSpeed = RunSpeed;
+                break;
+            case MoveState.Crouch:
+                MoveSpeed = CrouchSpeed;
+                break;
+            default:
+                MoveSpeed = WalkSpeed;
+                break;
+        }
+
+    }
+    void RotationInput()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
+    }
+
+    void Animate()
+    {
+        bool Moving = movement.magnitude > 0.1f;
+        animator.SetBool("IsMoving", Moving);
+
+        if (!Moving)
+        {
+            animator.enabled = false;
+            spriteRenderer.sprite = IdleSprite;
+        }
+        else
+        {
+            animator.enabled = true;
+        }
     }
 }
