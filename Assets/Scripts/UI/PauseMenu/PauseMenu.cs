761ddc9b43f03790ce6ed62c5d8965216002913c
SabrinaHarron <sabririnah2004@gmail.com>
2025-05-20 09:41:22 +0200

    New UI

---
+++ Assets/Scripts/UI/PauseMenu/PauseMenu.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        // Show pause menu UI
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        // Hide pause menu UI
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+        // Restart the game
+   
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+        // Load the main menu scene
+    }
+
+}
